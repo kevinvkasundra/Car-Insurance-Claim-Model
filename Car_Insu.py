82efbc2a639879ad01ef57ae0b8722b780495b3f
Kevin Vinay Kasundra <46608418+kevinvkasundra@users.noreply.github.com>
2019-05-16 19:55:59 -0500

    Code

---
+++ Car_Insu.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 10:29:09 2019
+
+@author: Kevin
+"""
+
+#Import the desired libraries.
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+from pylab import rcParams
+import tensorflow as tf
+from keras.models import Model, load_model
+from keras.layers import Input, Dense
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras import regularizers
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, precision_recall_curve
+from sklearn.metrics import recall_score, classification_report, auc, roc_curve
+from sklearn.metrics import precision_recall_fscore_support, f1_score
+from numpy.random import seed
+seed(1)
+from tensorflow import set_random_seed
+set_random_seed(2)
+SEED = 123 #used to help randomly select the data points
+DATA_SPLIT_PCT = 0.2
+rcParams['figure.figsize'] = 8, 6
+LABELS = ["Normal","Break"]
+
+
+#Data preprocessing
+df = pd.read_csv("train_set0506.csv")
+df.columns
+df = df.rename(columns = {'Claim_Amount':'y'})
+df = df.drop(['Unnamed: 0', 'Household_ID', 'Vehicle', 'Calendar_Year', 'Model_Year',
+       'Blind_Make', 'Blind_Model', 'Blind_Submodel', 'Cat1', 'Cat2', 'Cat3',
+       'Cat4', 'Cat5', 'Cat6', 'Cat7', 'Cat8', 'Cat9', 'Cat10', 'Cat11',
+       'Cat12', 'OrdCat','NVCat'], axis=1)
+for i in range(df.shape[0]):
+    if df.y.loc[i]>0:
+         df.y.loc[i] = 1
+    else: 
+        df.y.loc[i] = 0
+
+df_train, df_test = train_test_split(df, test_size=DATA_SPLIT_PCT, random_state=SEED)
+df_train, df_valid = train_test_split(df_train, test_size=DATA_SPLIT_PCT, random_state=SEED)
+df_train_0 = df_train.loc[df['y'] == 0]
+df_train_1 = df_train.loc[df['y'] == 1]
+df_train_0_x = df_train_0.drop(['y'], axis=1)
+df_train_1_x = df_train_1.drop(['y'], axis=1)
+df_valid_0 = df_valid.loc[df['y'] == 0]
+df_valid_1 = df_valid.loc[df['y'] == 1]
+df_valid_0_x = df_valid_0.drop(['y'], axis=1)
+df_valid_1_x = df_valid_1.drop(['y'], axis=1)
+df_test_0 = df_test.loc[df['y'] == 0]
+df_test_1 = df_test.loc[df['y'] == 1]
+df_test_0_x = df_test_0.drop(['y'], axis=1)
+df_test_1_x = df_test_1.drop(['y'], axis=1)
+'''
+time = df_train_0_x.time
+df_train_0_x=df_train_0_x.drop(['time'],axis=1)
+
+time1 = df_valid_0_x.time
+df_valid_0_x=df_valid_0_x.drop(['time'],axis=1)
+
+
+time2 = df_valid.time
+df_valid=df_valid.drop(['time'],axis=1)
+
+time3 = df_test_0_x.time
+df_test_0_x=df_test_0_x.drop(['time'],axis=1)
+
+
+time4 = df_test.time
+df_test=df_test.drop(['time'],axis=1)
+'''
+#Standardization
+scaler = StandardScaler().fit(df_train_0_x)
+df_train_0_x_rescaled = scaler.transform(df_train_0_x)
+scaler1 = StandardScaler().fit(df_valid_0_x)
+df_valid_0_x_rescaled = scaler1.transform(df_valid_0_x)
+scaler2 = StandardScaler().fit(df_valid.drop(['y'], axis = 1))
+df_valid_x_rescaled = scaler2.transform(df_valid.drop(['y'], axis = 1))
+
+scaler3 = StandardScaler().fit(df_test_0_x)
+df_test_0_x_rescaled = scaler3.transform(df_test_0_x)
+scaler4 = StandardScaler().fit(df_test.drop(['y'], axis = 1))
+df_test_x_rescaled = scaler4.transform(df_test.drop(['y'], axis = 1))
+
+###Autoencoder Classifier
+#Initialization
+nb_epoch = 1000
+batch_size = 128
+input_dim = df_train_0_x_rescaled.shape[1] #num of predictor variables, 
+encoding_dim = 6
+hidden_dim = int(encoding_dim / 2)
+learning_rate = 1e-3
+
+input_layer = Input(shape=(input_dim, )) #returns a tensor
+encoder = Dense(encoding_dim, activation="tanh", 
+                activity_regularizer=regularizers.l1(learning_rate))(input_layer)
+encoder = Dense(hidden_dim, activation="relu")(encoder)
+decoder = Dense(hidden_dim, activation='tanh')(encoder)
+decoder = Dense(input_dim, activation='relu')(decoder)
+autoencoder = Model(inputs=input_layer, outputs=decoder)
+
+#Training
+autoencoder.compile(metrics=['accuracy'],
+                    loss='mean_squared_error',
+                    optimizer='adam')
+cp = ModelCheckpoint(filepath="autoencoder_classifier.h5",
+                               save_best_only=True,
+                               verbose=0)
+tb = TensorBoard(log_dir='./logs',
+                histogram_freq=0,
+                write_graph=True,
+                write_images=True)
+history = autoencoder.fit(df_train_0_x_rescaled, df_train_0_x_rescaled,
+                    epochs=nb_epoch,
+                    batch_size=batch_size,
+                    shuffle=True,
+                    validation_data=(df_valid_0_x_rescaled, df_valid_0_x_rescaled),
+                    verbose=1,
+                    callbacks=[cp, tb]).history
+                          
+#Classification
+valid_x_predictions = autoencoder.predict(df_valid_x_rescaled)
+mse = np.mean(np.power(df_valid_x_rescaled - valid_x_predictions, 2), axis=1)
+error_df = pd.DataFrame({'Reconstruction_error': mse,
+                        'True_class': df_valid['y']})
+precision_rt, recall_rt, threshold_rt = precision_recall_curve(error_df.True_class, error_df.Reconstruction_error)
+plt.plot(threshold_rt, precision_rt[1:], label="Precision",linewidth=5)
+plt.plot(threshold_rt, recall_rt[1:], label="Recall",linewidth=5)
+plt.title('Precision and recall for different threshold values')
+plt.xlabel('Threshold')
+plt.ylabel('Precision/Recall')
+plt.legend()
+plt.show()
+
+test_x_predictions = autoencoder.predict(df_test_x_rescaled)
+mse = np.mean(np.power(df_test_x_rescaled - test_x_predictions, 2), axis=1)
+error_df_test = pd.DataFrame({'Reconstruction_error': mse,
+                        'True_class': df_test['y']})
+error_df_test = error_df_test.reset_index()
+threshold_fixed = .55
+groups = error_df_test.groupby('True_class')
+fig, ax = plt.subplots()
+for name, group in groups:
+    ax.plot(group.index, group.Reconstruction_error, marker='o', ms=3.5, linestyle='',
+            label= "Break" if name == 1 else "Normal")
+ax.hlines(threshold_fixed, ax.get_xlim()[0], ax.get_xlim()[1], colors="r", zorder=100, label='Threshold')
+ax.legend()
+plt.title("Reconstruction error for different classes")
+plt.ylabel("Reconstruction error")
+plt.xlabel("Data point index")
+plt.show();
+
+pred_y = [1 if e > threshold_fixed else 0 for e in error_df.Reconstruction_error.values]
+conf_matrix = confusion_matrix(error_df.True_class, pred_y)
+plt.figure(figsize=(12, 12))
+sns.heatmap(conf_matrix, xticklabels=LABELS, yticklabels=LABELS, annot=True, fmt="d");
+plt.title("Confusion matrix")
+plt.ylabel('True class')
+plt.xlabel('Predicted class')
+plt.show()
